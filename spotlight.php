69541c6e09c84a182b1f3661683a5a663cd21a30
Victor Opere <victoropere@gmail.com>
2020-05-09 14:22:48 +0300

    added the deffering of JS Code

--- spotlight.php
+++ spotlight.php
@@ -33,6 +33,21 @@ function register_spotlightpopularposts_widget(){
   // Hook in function
 add_action('widgets_init', 'register_spotlightpopularposts_widget');
 
+
+// to defer parsing of JS
+
+function spotlight_defer_parsing_of_js ( $url ) {
+
+    if ( FALSE === strpos( $url, '.js' ) ) return $url;
+    if ( strpos( $url, 'jquery.js' ) ) return $url;
+    return "$url' defer ";
+
+  }
+
+  add_filter( 'clean_url', 'defer_parsing_of_js', 11, 1 );
+
+  
+
 //  activation
 // require_once plugin_dir_path( __FILE__ ) . 'inc/dragonoid-activation.php';
 
